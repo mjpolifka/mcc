cc1ede31dbf161efc896b461c799a5237c6ead2f
Mitch Polifka <mjpolifka@gmail.com>
2026-05-22 09:22:22 -0500

    set up endpoints

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {res.send('Hello World!');})
+app.get('/', (req, res) => {res.send('I think I can serve the frontend from here later.');})
+
+app.get('/card/:id', (req, res)  => {
+  res.send(`card id: ${req.params['id']}`);
+})
+
+app.get('/deck/:id', (req, res) => {
+  res.send(`deck id: ${req.params['id']}`);
+})
+
+app.get('/banlist', (req, res) => {
+  res.send('banlist coming soon');
+})
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}`);})
