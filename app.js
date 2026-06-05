d0b75964f227378d80b51c0edc08e54c8894829e
Mitch Polifka <mjpolifka@gmail.com>
2026-06-05 12:00:36 -0500

    created `card` route

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const port = 3000;
 
 app.get('/', (req, res) => {res.send('I think I can serve the frontend from here later.');})
 
-app.get('/card/:id', (req, res)  => {
-  res.send(`card id: ${req.params['id']}`);
+app.get('/card/:id', async (req, res)  => {
+  // TODO: validate input before using it
+  const data = await getCardStatus(req.params['id']);
+  res.send(`card data: ${data}`);
 })
 
 app.get('/deck/:id', (req, res) => {
@@ -17,3 +19,25 @@ app.get('/banlist', (req, res) => {
 })
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}`);})
+
+
+
+async function getCardStatus(id) {
+  const url = 'https://api.scryfall.com/cards/named?exact=' + id;
+  const response = await fetch(
+    url,
+    {
+      method: 'GET',
+      headers: {
+        'User-Agent': 'MiddleClassCommander/1.0',
+        'Accepts': 'application/json',
+      },
+    });
+  
+  if (!response.ok) {
+    throw new Error(`Bad Scryfall response: ${response.status}, ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data['oracle_id'];
+}
